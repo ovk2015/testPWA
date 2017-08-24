45ea72167113c801bb2e2bc0e59f77a007ff012f
ovk2015 <31317656+ovk2015@users.noreply.github.com>
2017-08-25 00:37:51 +0300

    Add files via upload

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('Работаем!!!!')
+var path2root = 'http://localhost/kwork/RudenkoMaxim/PWA/testPWA/';
+
+if('serviceWorker' in navigator)
+{
+	navigator.serviceWorker.register(path2root + 'js/SW.js', {scope: path2root}).
+		then(function(reg) 
+		{			
+			console.log('Зарегестрировались ' + reg.scope);
+		}).
+		catch(function(error) 
+		{			
+			console.log('Ошибка регистрации ' + error);
+		});
+};
