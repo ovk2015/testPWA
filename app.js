fbccd4b5867742222059d6c20b22c88353613455
ovk2015 <31317656+ovk2015@users.noreply.github.com>
2017-08-25 15:36:49 +0300

    Add files via upload

---
+++ app.js
@@ -0,0 +1,14 @@
+var path2root = 'https://ovk2015.github.io/testPWA/';
+
+if('serviceWorker' in navigator)
+{
+	navigator.serviceWorker.register(path2root + 'js/SW.js', {scope: path2root}).
+		then(function(reg) 
+		{			
+			console.log('Зарегестрировались ' + reg.scope);
+		}).
+		catch(function(error) 
+		{			
+			console.log('Ошибка регистрации ' + error);
+		});
+};
